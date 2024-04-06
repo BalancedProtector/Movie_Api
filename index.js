c6ae1ee28b8b1c6ea65ae935a0299bfc258d2325
BalancedProtector <139424334+BalancedProtector@users.noreply.github.com>
2024-04-06 14:26:12 -0400

    Lesson 2.5 Task

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ const express = require("express"),
     fs = require("fs"),
     path = require("path"),
     bodyParser = require("body-parser"),
-    methodOverride = require("method-override");
+    methodOverride = require("method-override"),
+    uuid = require("uuid");
 const app = express();
 //Logging Middleware
 const accessLogStream = fs.createWriteStream(path.join(__dirname, "log.txt"), {flags: "a"});
@@ -22,14 +23,48 @@ app.use(morgan("combined" , {stream: accessLogStream}));
     app.use(express.static("public"));
 //get requests
 app.get ("/", (req, res) => {
+    res.status(200).send("Welcome to Mind Theatre!");
     res.sendFile("/index.html", {root: __dirname});
 });
 app.get("/movies", (req, res) => {
     res.sendFile("/public/movies.json", {root: __dirname});
 })
+app.get("/movies/:title", (req, res) => {
+    res.send("Sends data about a specific movie");
+    return res.status(200).send(req.params.title);
+})
+app.get("/movies/:genre", (req, res) => {
+    res.send("sends data about all movies in a specific genre");
+    return res.status(200).send(req.params.genre + " movies");
+})
+app.get("/movies/:director", (req, res) => {
+    res.send("Sends data about all the movies directed by a specific director");
+    return res.status(200).send("Movies directed by " + req.params.director);
+})
+app.get("/movies/:year", (req, res) => {
+    res.send("sends data about all movies directed in a given year");
+    return res.status(200).send("Movies released in " + req.params.year);
+})
+app.get("/movies/:genre/:year", (req, res) => {
+    res.send("Sends data about all movies in a given genre released in a given year");
+    return res.status (200).send(req.params.genre + " movies released in " + req.params.year);
+})
+app.get("/directors", (req, res) => {
+    res.send("Sends list of all directors");
+    return res.status(200).send("List of Directors");
+})
+app.get("/genres", (req, res) => {
+    res.send ("Sends list of all genres");
+    return res.status(200).send("List of Genres");
+})
 app.get("/documentation", (req, res) => {
     res.sendFile("/public/documentation.html", {root: __dirname});
 });
+//post requests
+app.post("/movies", (req, res) => {
+    res.send("Adds a new movie to the list of movies");
+    return res.status(201).send("New Movie Added");
+})
 //Server Port
 app.listen(8080, () => {
     console.log("Server is running on http://localhost:8080");
