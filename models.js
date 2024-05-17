62cfa57e7650853298f5970ab8c895b005d04f04
BalancedProtector <139424334+BalancedProtector@users.noreply.github.com>
2024-05-17 16:31:50 -0400

    hashPass err

--- models.js
+++ models.js
@@ -23,11 +23,11 @@ let userSchema = mongoose.Schema({
     Birthday: Date,
     favoriteMovies: [ { type : mongoose.Schema.Types.ObjectId, ref : 'Movie'} ]
 });
-userSchema.statistics.hashPassword = (password) => {
-    return bcrypt.hashSync(password, 10);
+userSchema.statistics.hashPassword = (Password) => {
+    return bcrypt.hashSync(Password, 10);
 }
-userSchema.methods.validatePassword = function(password) {
-    return bcrypt.compareSync(password, this.Password);
+userSchema.methods.validatePassword = function(Password) {
+    return bcrypt.compareSync(Password, this.Password);
 }
 
 let Movie = mongoose.model('Movie', movieSchema);
